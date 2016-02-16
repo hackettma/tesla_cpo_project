41829f0321d21d84a559cfc7b9c614876b3e2094
Michael Hackett <hackettma@gmail.com>
2016-02-15 20:25:06 -0600

    Changing Format to web_app

--- database_edit.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as lite
-
-con = lite.connect('temp.db', detect_types=lite.PARSE_DECLTYPES|lite.PARSE_COLNAMES)
-with con:
-	cur = con.cursor()
-	cur.execute("DROP TABLE IF EXISTS Cars")
-	cur.execute("CREATE TABLE Cars(Id TEXT, Created_at timestamp, Last_Update timestamp, Status TEXT, Location TEXT, Year TEXT, Model TEXT, Mileage FLOAT, Price FLOAT, Paint TEXT, Interior TEXT, Roof TEXT, Wheel TEXT)")
